42cafe70364a69bdc68081aac1a61890a09e7bb4
RyanPrasetyaa <yiyan125@gmail.com>
2025-10-03 08:58:11 +0700

    making checkout page

--- eggsplore/lib/app_routes.dart
+++ eggsplore/lib/app_routes.dart
@@ -20,6 +20,7 @@ import 'package:flutter/material.dart';
 import 'package:eggsplore/pages/cart_page.dart';
 import 'package:eggsplore/pages/detail_product_page.dart';
 import 'package:eggsplore/pages/chat_page.dart';
+import 'package:eggsplore/pages/checkout_page.dart';
 
 class AppRoutes {
   static const String welcome = '/';
@@ -43,6 +44,7 @@ class AppRoutes {
   static const String shopProfile = '/shop-profile';
   static const String detailProduct = '/detail-product';
   static const String chat = '/chat';
+  static const String checkout = '/checkout';
 
   static Map<String, WidgetBuilder> routes = {
     welcome: (context) => const WelcomeScreen(),
@@ -66,6 +68,6 @@ class AppRoutes {
     shopProfile: (context) => const ShopProfilePage(),
     detailProduct: (context) => const DetailProductPage(),
     chat: (context) => const ChatPage(),
-
+    checkout: (context) => const CheckoutPage(),
   };
 }

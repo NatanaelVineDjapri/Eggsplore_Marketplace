7eaf0742c50f644de2fb11e8113382b05d109d6d
ClaudioTaffarel <claudio.taffarel889@gmail.com>
2025-10-05 15:35:13 +0700

    Resize the length of Shop Actions Card

--- eggsplore/lib/main.dart
+++ eggsplore/lib/main.dart
@@ -28,7 +28,7 @@ class _MyAppState extends ConsumerState<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.profile,
+      initialRoute: AppRoutes.myshop,
       routes: AppRoutes.routes,
     );
   }
